627ede7a71667e7ada2a715c8c5247eaa5a25f2f
Chih-Hsiang, Chen <chih.hsi.chen@gmail.com>
2020-12-31 00:06:50 +0800

    change 'mci' to 'mvn clean install'

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd p4extensiontreatment
-mci -DskipTests
+mvn clean install -DskipTests
 mvn install:install-file -Dfile=target/p4extensiontreatment-1.0-SNAPSHOT.jar -DgroupId=nctu.pncourse -DartifactId=p4extensiontreatment -Dversion=1.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
 
 cd ../pipeconf-p4-final
-mci -DskipTests
+mvn clean install -DskipTests
 
 cd ../vxlan-app
-mci -DskipTests
+mvn clean install -DskipTests
